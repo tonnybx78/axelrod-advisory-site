4a598484b104162aad4112445378ddbf19da9470
tonnybx78 <tonnybx@gmail.com>
2025-09-28 15:48:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Services from './components/Services'
-import HowWeWork from './components/HowWeWork'
-import References from './components/References'
-import Insights from './components/Insights'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import HowWeWork from './components/HowWeWork';
+import References from './components/References';
+import Insights from './components/Insights';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-export default function App(){
+function App() {
   return (
     <>
       <Header />
@@ -21,5 +21,7 @@ export default function App(){
       <Contact />
       <Footer />
     </>
-  )
+  );
 }
+
+export default App;
